189924bac47e70cbf29c9e84cd1a6e3419413c5e
ParaN3xus <136563585+ParaN3xus@users.noreply.github.com>
2025-07-31 09:40:53 +0800

    build: bump version to v0.13.18 (#2)
    * build: use nightly typst, typst-ansi-hl, typstyle, reflexo; update ttf-parser,fontdb
    
    * refactor: new eval_string api
    
    * refactor: typst::html -> typst_html
    
    * refactor: new Source lines api in typst/typst/6308
    
    * refactor: new ast::Expr enum names in typst/typst#5784
    
    * refactor: new color api in typst/typst#6479
    
    * refactor: new element api in typst/typst#6547
    
    * refactor: new html frame api in typst/typst#6505
    
    * refactor: new label constructing api in typst/typst#6332
    
    * refactor: new symbol api in typst/typst#6441
    
    * fix: catch_unwind when printing Ty's debug format
    
    * styl: fmt
    
    * refactor: use new LibraryExt mod
    
    * fix: increase recursion limit for tinymist-query
    
    * tests: update test snaps
    
    * ci: continue on error
    
    * deps: update rustc
    
    * tests: update test snaps
    
    * deps: patch reflexo
    
    * refactor(typlite): replace manual highlight and strike with official impl
    
    * tests: update test snaps for shortened empty attributes syntax
    
    * tests(typlite): update test snaps to ignore outline temporarily
    
    * fix(typlite): convert link
    
    * feat: auto release nightly
    
    * feat: auto tag stable release

--- tests/e2e/main.rs
+++ tests/e2e/main.rs
@@ -377,7 +377,7 @@ fn e2e() {
         });
 
         let hash = replay_log(&tinymist_binary, &root.join("neovim"));
-        insta::assert_snapshot!(hash, @"siphash128_13:7e4a9cdceda2bf7409ec907108d0f927");
+        insta::assert_snapshot!(hash, @"siphash128_13:1aa2e6cae480307559aadb11636169ca");
     }
 
     {
@@ -388,7 +388,7 @@ fn e2e() {
         });
 
         let hash = replay_log(&tinymist_binary, &root.join("vscode"));
-        insta::assert_snapshot!(hash, @"siphash128_13:b18199e96fd07287ead7caa2b33f777");
+        insta::assert_snapshot!(hash, @"siphash128_13:3b9d66e76f15674d5a94cd66f7bff2e8");
     }
 }
 
