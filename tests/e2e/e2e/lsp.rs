8de370329e206e75a7516934947382d8de73029b
ParaN3xus <136563585+ParaN3xus@users.noreply.github.com>
2025-08-08 17:22:12 +0800

    dev: update nightly (#2018)
    - fix typlite tests
    - update test snaps

--- tests/e2e/e2e/lsp.rs
+++ tests/e2e/e2e/lsp.rs
@@ -22,7 +22,7 @@ fn test_lsp() {
         });
 
         let hash = replay_log(&root.join("neovim"));
-        insta::assert_snapshot!(hash, @"siphash128_13:6748f1ff6d06426aa788a0683b6146d5");
+        insta::assert_snapshot!(hash, @"siphash128_13:f9ba12e829fe6e5cb16d16ef310c4643");
     }
 
     {
@@ -33,7 +33,7 @@ fn test_lsp() {
         });
 
         let hash = replay_log(&root.join("vscode"));
-        insta::assert_snapshot!(hash, @"siphash128_13:5019cb39687d1a16853bc80320d00e26");
+        insta::assert_snapshot!(hash, @"siphash128_13:7eced41bc62b008708a200ec68ca4cb4");
     }
 }
 
